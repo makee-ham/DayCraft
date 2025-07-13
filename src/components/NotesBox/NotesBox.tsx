e1b4f08a4c0a93107bce35cf344d27ef42e0a3d6
Makee <ina0312@naver.com>
2025-07-13 21:38:46 +0900

    feat #4 set features of notesbox and styled it

--- src/components/NotesBox/NotesBox.tsx
+++ src/components/NotesBox/NotesBox.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 export default function NotesBox() {
+  const [note, setNote] = useState<string>("");
+
+  const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setNote(e.target.value);
+
   return (
-    <section>
-      <textarea />
+    <section className="w-full max-w-sm rounded-box overflow-hidden">
+      <h2 className="text-lg font-bold text-center py-2 bg-primary text-primary-content">
+        NOTES
+      </h2>
+      <form
+        className="border border-base-300"
+        style={{
+          borderBottomLeftRadius: "var(--radius-box)",
+          borderBottomRightRadius: "var(--radius-box)",
+        }}
+      >
+        <textarea
+          value={note}
+          onChange={handleNoteChange}
+          placeholder="Any thoughts, reflections, or feelings..."
+          className="w-full p-2 min-h-[6rem] resize-none textarea-sm textarea-neutral outline-accent"
+        />
+      </form>
     </section>
   );
 }
