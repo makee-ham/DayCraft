283666d5d2a7d0769b97705520d4770220dd40b3
Makee <ina0312@naver.com>
2025-07-14 17:34:05 +0900

    feat #5 made useWeather hook

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,3 +1,45 @@
-// const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API key}&units=metric&lang=en`;
+import { useEffect, useState } from "react";
 
-export default function useWeather() {}
+const APIkey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
+export default function useWeather() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported.");
+      setLoading(false);
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          const res = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}&units=metric&lang=en`
+          );
+          if (!res.ok) throw new Error("Failed to fetch weather.");
+          const json = await res.json();
+          setData(json);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            setError(err.message);
+          } else {
+            setError(String(err));
+          }
+        } finally {
+          setLoading(false);
+        }
+      },
+      (err) => {
+        setError(err.message);
+        setLoading(false);
+      }
+    );
+  }, []);
+
+  return { data, loading, error };
+}
