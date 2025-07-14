f1d32b9c7944bc677b8e13d6e63c2f87361a6dfc
Makee <ina0312@naver.com>
2025-07-14 17:47:28 +0900

    feat #5 make constants incase location access is denied

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -2,10 +2,33 @@ import { useEffect, useState } from "react";
 
 const APIkey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
+// default coordinates-- Bundang-Gu, Seongnam City
+const FALLBACK_LAT = 37.3827531654052;
+const FALLBACK_LON = 127.118829944284;
+
 export default function useWeather() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+
+  const fetchWeather = async (lat: number, lon: number) => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric&lang=en`
+      );
+      if (!res.ok) throw new Error("Failed to fetch weather.");
+      const json = await res.json();
+      setData(json);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError(String(err));
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (!navigator.geolocation) {
@@ -17,26 +40,16 @@ export default function useWeather() {
     navigator.geolocation.getCurrentPosition(
       async (position) => {
         const { latitude, longitude } = position.coords;
-        try {
-          const res = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}&units=metric&lang=en`
-          );
-          if (!res.ok) throw new Error("Failed to fetch weather.");
-          const json = await res.json();
-          setData(json);
-        } catch (err: unknown) {
-          if (err instanceof Error) {
-            setError(err.message);
-          } else {
-            setError(String(err));
-          }
-        } finally {
+        await fetchWeather(latitude, longitude);
+      },
+      async (err) => {
+        if (err.code === 1) {
+          console.warn("Geolocation denied, using default coordinates.");
+          await fetchWeather(FALLBACK_LAT, FALLBACK_LON);
+        } else {
+          setError(err.message);
           setLoading(false);
         }
-      },
-      (err) => {
-        setError(err.message);
-        setLoading(false);
       }
     );
   }, []);
